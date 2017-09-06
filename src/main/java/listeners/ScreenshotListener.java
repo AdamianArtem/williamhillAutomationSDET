8b97788472f6d33d2bb11b35d81a7e157766cb88
Artem <aadamian@marketingg2.com>
2017-09-06 21:27:21 +0300

    fix ScreenshotListener

--- src/main/java/listeners/ScreenshotListener.java
+++ src/main/java/listeners/ScreenshotListener.java
@@ -1,12 +1,13 @@
 package listeners;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 
+import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
-import ru.yandex.qatools.allure.annotations.Attachment;
 
 public class ScreenshotListener extends TestListenerAdapter {
 
--- src/main/java/runners/CucumberRunner.java
+++ src/main/java/runners/CucumberRunner.java
@@ -1,19 +1,12 @@
 package runners;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import listeners.ScreenshotListener;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
-import ru.yandex.qatools.allure.Allure;
-import ru.yandex.qatools.allure.annotations.Attachment;
-
-import static com.codeborne.selenide.Selenide.open;
 
 @CucumberOptions(
 		features = "src/main/java/features",
@@ -28,6 +21,7 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
 	public void setUp() throws Exception {
         ChromeDriverManager.getInstance().setup();
 		Configuration.browser = "chrome";
+		Configuration.screenshots = false;
 //		open("http://automated-testing.info/t/gde-v-otchete-allure-dolzhen-poyavitsya-skrinshot/5924");
 //        saveAllureScreenshot();
 
